d44a3eaa7a22cc8d6c578fac0ee8c81141337446
GarudaFX <francisjohnt24@gmail.com>
2023-11-21 18:43:20 +0800

    do-track frontend

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='grid place-items-center justify-center h-screen'>
+			<h1 className='text-6xl font-bold text-center'>Do-Track Front-end Setup</h1>
+		</div>
+	);
+}
+
+export default App;
