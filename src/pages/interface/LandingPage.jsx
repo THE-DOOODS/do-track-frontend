9fcf2df7ba4d8344253a255c01706cc65edf137e
Angelo Galope <105281585+angelogalope@users.noreply.github.com>
2023-12-16 02:32:52 +0800

    Feat: Added Mockup and White Logo

--- src/pages/interface/LandingPage.jsx
+++ src/pages/interface/LandingPage.jsx
@@ -3,7 +3,7 @@ const LandingPage = () => {
     return (
         <div className="flex flex-col justify-center items-center gap-6 bg-gradient-to-b from-primPurple to-primOrange h-screen w-full p-4">
             <div className="flex flex-col items-center w-auto bg-white bg-opacity-40 p-10 rounded-xl relative z-10">
-                <img src="./public/static/icons/Logo(white).png" alt="" className="w-[164px] md:w-[264px]" />
+                <img src="/static/icons/Logo(white).png" alt="" className="w-[164px] md:w-[264px]" />
                 <div className="flex flex-col items-center gap-8">
                     <h1 className="text-auto md:text-2xl text-center w-auto lg:w-[534px] text-white">
                         Do-Day Activity Attendance and Management System
@@ -20,7 +20,7 @@ const LandingPage = () => {
                 alt=""
                 className="h-[422px] absolute -rotate-12 -translate-x-80 hidden lg:block"
             />
-        </div>
+        </div>  
     );
 };
 
