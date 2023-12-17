43e83249b8bdc49c0ef3d9336846036ec222ea66
Angelo Galope <105281585+angelogalope@users.noreply.github.com>
2023-12-17 12:51:13 +0800

    Feat: Added logout confirmation

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,50 +1,78 @@
-import { useState } from "react";
-import { RiArrowDropDownLine } from "react-icons/ri";
-import { IoMdExit } from "react-icons/io";
+import React, { useRef, useState } from 'react';
+import { RiArrowDropDownLine } from 'react-icons/ri';
+import { IoMdExit } from 'react-icons/io';
+import { useNavigate } from "react-router";
+import Logout from './modals/Logout';
+import LoadingBar from 'react-top-loading-bar';
+import { Toaster, toast } from 'sonner';
 
 const Topbar = () => {
-    const [menu, setMenu] = useState(false);
-    
-    const first_name = localStorage.getItem("first_name");
-    const last_name = localStorage.getItem("last_name");
+  const [menu, setMenu] = useState(false);
+  const [isLogout, setIsLogout] = useState(false);
 
-    const toggleMenu = () => {
-        setMenu(!menu);
-    };
+  const navigator = useNavigate();
+  const loadingBar = useRef(null);
 
-    return (
-        <div className="flex items-center justify-between pb-3">
-            <img src="/static/icons/Logo.png" alt="Do-Track Logo" className="w-[116px]" />
-            <div className="flex items-center gap-3">
-                <h1 className="">{first_name} {last_name}</h1>
-                <div className="flex items-center gap-2 relative">
-                    <div className="w-[32px] h-[32px] rounded-full bg-primPurple"></div>
-                    <button
-                        onClick={toggleMenu}
-                        className="rounded-md bg-gray-200 p-1"
-                    >
-                        <RiArrowDropDownLine />
-                    </button>
-                    {menu && (
-                        <div className="z-50 absolute top-full right-0 mt-2 bg-gray-200 rounded-lg shadow w-44">
-                            <ul className="py-2 text-sm text-gray-700">
-                                <li>
-                                    <a href="#" className="flex px-4 py-2 hover:bg-gray-100 ">
-                                        Settings
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="/login" className="flex items-center gap-3 px-4 py-2 hover:bg-gray-100 ">
-                                        Sign Out <IoMdExit />
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    )}
-                </div>
+  const first_name = localStorage.getItem('first_name');
+  const last_name = localStorage.getItem('last_name');
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
+
+  const handleLogout = () => {
+    setIsLogout(true);
+  };
+
+  const handleCloseLogout = () => {
+    setIsLogout(false);
+  };
+
+  const handleConfirmLogout = () => {
+    loadingBar.current.continuousStart(60);
+    toast.success('Logging out...');
+    setTimeout(() => {
+        loadingBar.current.complete();
+        setTimeout(() => {
+            navigator("/login");
+            setIsLogout(false);
+        }, 1200);
+    }, 1000);
+  };
+
+  return (
+    <div className="flex items-center justify-between pb-3">
+      <Toaster position='top-right' closeButton richColors />
+      <LoadingBar height={7} color="#4ab516" ref={loadingBar} />
+      {isLogout && <Logout onClose={handleCloseLogout} onConfirm={handleConfirmLogout} />}
+      <img src="/static/icons/Logo.png" alt="Do-Track Logo" className="w-[116px]" />
+      <div className="flex items-center gap-3">
+        <h1>{first_name} {last_name}</h1>
+        <div className="flex items-center gap-2 relative">
+          <div className="w-[32px] h-[32px] rounded-full bg-primPurple"></div>
+          <button onClick={toggleMenu} className="rounded-md bg-gray-200 p-1">
+            <RiArrowDropDownLine />
+          </button>
+          {menu && (
+            <div className="z-40 absolute top-full right-0 mt-2 bg-gray-200 rounded-lg shadow w-44">
+              <ul className="py-2 text-sm text-gray-700">
+                <li>
+                  <button href="#" className="flex px-4 py-2 hover:bg-gray-100 w-full">
+                    Settings
+                  </button>
+                </li>
+                <li>
+                  <button onClick={handleLogout} className="flex items-center gap-3 px-4 py-2 hover:bg-gray-100 w-full">
+                    Sign Out <IoMdExit />
+                  </button>
+                </li>
+              </ul>
             </div>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
---
+++ src/components/modals/Logout.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const Logout = ({ onClose, onConfirm }) => {
+  return (
+    <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-60 flex">
+      <div className="items-center justify-center p-8 gap-4 bg-white w-full max-w-md m-auto flex-col flex rounded-lg shadow">
+        <h1 className="text-lg font-medium text-gray-600">Are you sure you want to logout?</h1>
+        <div className="flex flex-row gap-3">
+          <button onClick={onClose} className="text-gray-600 font-medium">
+            Cancel
+          </button>
+          <button onClick={onConfirm} className="text-red-500 font-medium">
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logout;
