d225c499a67028fc8bbbb51af783d849b5fe8f8a
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-20 11:31:39 +0800

    update: added route prtoection for dashboard

--- src/components/StudentLists.jsx
+++ src/components/StudentLists.jsx
@@ -6,12 +6,16 @@ import { useState, useEffect, useRef } from "react";
 import JsPDF, { jsPDF } from "jspdf";
 import html2canvas from "html2canvas";
 
+import { IoIosSearch } from "react-icons/io";
+
 const StudentLists = ({ programAttend, selectedProgram, allStudents }) => {
 	const college_id = localStorage.getItem("college_id");
 
 	const [collegeAttend, setCollegeAttend] = useState([]);
 	const [page, setPage] = useState(0);
 	const [rowsPerPage, setRowsPerPage] = useState(10);
+
+	const [searchID, setSearchID] = useState("");
 
 	const list = useRef(null);
 
@@ -55,6 +59,7 @@ const StudentLists = ({ programAttend, selectedProgram, allStudents }) => {
 		page * rowsPerPage + rowsPerPage,
 	);
 
+	// function for handling the creation of pdf list of all atudent attendees
 	const exportPDF = () => {
 		// Adjust the width and height based on your requirements
 		const pdfWidth = 210;
@@ -88,27 +93,40 @@ const StudentLists = ({ programAttend, selectedProgram, allStudents }) => {
 		});
 	};
 
+	// search functionalities
+	const handleSearchStudentAttendee = (event) => {
+		if (event.key === "Enter") {
+			// todo implement the search functionality here
+			//? overwrite the data that is being passed from params
+		}
+	};
+
 	return (
 		<div className="flex flex-col py-4 gap-3">
 			<div className="flex items-center justify-between">
 				<h1 className="text-xl font-bold text-gray-600">List of Attendees</h1>
-				<button
-					onClick={exportPDF}
-					className="flex items-center gap-2 border p-1 px-4 rounded-full text-sm text-white bg-primOrange h-10">
-					Convert List of PDF <TbClipboardList size={16} />
-				</button>
+				<div className="flex items-center gap-2">
+					<div className="relative flex items-center">
+						<input
+							type="text"
+							placeholder="Search student by ID no."
+							className="h-10 px-12 rounded-full outline-primPurple text-xs border border-gray-300 text-gray-500"
+							value={searchID}
+							onChange={(e) => setSearchID(e.target.value)}
+							onKeyDown={handleSearchStudentAttendee}
+						/>
+						<IoIosSearch
+							onClick={handleSearchStudentAttendee}
+							className="absolute left-3 text-gray-500"
+						/>
+					</div>
+					<button
+						onClick={exportPDF}
+						className="flex items-center gap-2 border p-1 px-4 rounded-full text-sm text-white bg-primOrange h-10">
+						Convert List of PDF <TbClipboardList size={16} />
+					</button>
+				</div>
 			</div>
-			{/* <div className="flex items-center gap-4 text-gray-700">
-                <button 
-                    onClick={handleAttendCollegeRequest}
-                    className="cursor-pointer text-primPurple font-bold  p-2 px-4 rounded-t-md rounded-tr-3xl bg-gray-200 shadow-slate-700 shadow-md">
-                        All Students
-                </button>
-                <h1 className="cursor-pointer">All Students</h1>
-                <h1 className="cursor-pointer">Inactive Students</h1>
-                <input type="text" placeholder="Search student" className="p-1 px-4 border rounded-full text-sm" />
-            </div>
-            <hr /> */}
 			<div ref={list} className="relative overflow-x-auto shadow-md sm:rounded-lg">
 				<table className="w-full text-sm text-left rtl:text-right text-gray-500">
 					<thead className="text-xs text-gray-700 uppercase bg-purple -200 bg-gray-200">
--- src/pages/authorization/Login.jsx
+++ src/pages/authorization/Login.jsx
@@ -2,8 +2,8 @@ import React, { useRef, useState } from "react";
 import { PiEyeBold, PiEyeClosedBold } from "react-icons/pi";
 import { IoClose } from "react-icons/io5";
 import { useNavigate } from "react-router";
-import { Toaster, toast } from 'sonner';
-import LoadingBar from 'react-top-loading-bar';
+import { Toaster, toast } from "sonner";
+import LoadingBar from "react-top-loading-bar";
 
 const Login = () => {
 	const [showPassword, setShowPassword] = useState(false);
@@ -23,13 +23,13 @@ const Login = () => {
 	const handleLoginRequest = async (event) => {
 		event.preventDefault();
 		loadingBar.current.continuousStart(60);
-		
+
 		setEmailError(false);
 		setPassError(false);
-		
+
 		const fields = [
-			{value: password, setError: setPassError, message: "Password is required"},
-			{value: email, setError: setEmailError, message: "Email is required"},
+			{ value: password, setError: setPassError, message: "Password is required" },
+			{ value: email, setError: setEmailError, message: "Email is required" },
 		];
 
 		let isValid = true;
@@ -46,27 +46,30 @@ const Login = () => {
 		// if (email === "") {
 		// 	toast.error('Email is required')
 		// } else if (password === "") {
-			// 	toast.error('Password is required')
+		// 	toast.error('Password is required')
 		// } else
-		
+
 		if (isValid) {
 			if (email !== "" && !emailRegex.test(email)) {
 				loadingBar.current.complete();
-				toast.error('Please use university email')
+				toast.error("Please use university email");
 				setEmailError(true);
 			} else {
 				try {
-					let response = await fetch("https://do-track-backend-production.up.railway.app/api/login", {
-						method: "POST",
-						headers: {
-							"Content-Type": "application/json",
+					let response = await fetch(
+						"https://do-track-backend-production.up.railway.app/api/login",
+						{
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								email,
+								password,
+							}),
 						},
-						body: JSON.stringify({
-							email,
-							password,
-						}),
-					});
-					
+					);
+
 					if (response.ok) {
 						const data = await response.json();
 						localStorage.setItem("admin_id", data.data?.admin?.admin_id);
@@ -75,40 +78,41 @@ const Login = () => {
 						localStorage.setItem("first_name", data.data?.admin?.first_name);
 						localStorage.setItem("last_name", data.data?.admin?.last_name);
 						localStorage.setItem("position", data.data?.admin?.position);
-						
+						localStorage.setItem("token", data.data?.token);
+
 						toast.promise(promise, {
-							loading: 'Logging in...',
+							loading: "Logging in...",
 							success: () => {
-							  return `Success Logging In`;
+								return `Success Logging In`;
 							},
-							error: 'Error',
+							error: "Error",
 						});
 						setTimeout(() => {
 							loadingBar.current.complete();
 							setTimeout(() => {
 								navigator("/dashboard");
-							}, 1200)
-						},1000);
+							}, 1200);
+						}, 1000);
 					} else {
 						if (response.status === 401) {
 							const data = await response.json();
 							if (data.message === "Admin not found") {
 								loadingBar.current.complete();
 								setEmailError(true);
-								toast.error('User does not exist');
+								toast.error("User does not exist");
 							}
 						} else if (response.status === 403) {
 							const data = await response.json();
 							if (data.message === "Invalid password. Please try again.") {
 								loadingBar.current.complete();
 								setPassError(true);
-								toast.error('Incorrect password');
+								toast.error("Incorrect password");
 							}
 						}
 					}
 				} catch (err) {
 					loadingBar.current.complete();
-					toast.warning('Internal Server Error')
+					toast.warning("Internal Server Error");
 					console.log(err);
 				}
 			}
@@ -120,7 +124,11 @@ const Login = () => {
 			<LoadingBar height={7} color="#4ab516" ref={loadingBar} />
 			<Toaster closeButton position="top-center" richColors />
 			<div className="lg:w-full hidden lg:flex justify-end items-end">
-				<img src="/static/images/login_illustrator.png" alt="" className="w-[700px]" />
+				<img
+					src="/static/images/login_illustrator.png"
+					alt=""
+					className="w-[700px]"
+				/>
 			</div>
 			<div className="flex flex-col justify-between lg:justify-center w-[332px] lg:w-full h-[524px] lg:h-full rounded-lg lg:rounded-none p-5 lg:p-10 bg-white">
 				<div className="flex flex-row justify-between items-start mx-0 lg:mx-20">
@@ -129,7 +137,7 @@ const Login = () => {
 					</div>
 					<div className="flex items-center">
 						<a href="/">
-						<IoClose className="text-primPurple" size={32} />
+							<IoClose className="text-primPurple" size={32} />
 						</a>
 					</div>
 				</div>
@@ -142,11 +150,10 @@ const Login = () => {
 				<form>
 					<div className="flex flex-col mx-0 lg:mx-20 pt-4 gap-5">
 						<div className="flex flex-col gap-1">
-							<label className={`${
-								emailError
-									? "text-red-500 font-medium"
-									: "text-gray-500 font-medium"
-							}`}>
+							<label
+								className={`${
+									emailError ? "text-red-500 font-medium" : "text-gray-500 font-medium"
+								}`}>
 								Email
 							</label>
 							<input
@@ -162,11 +169,10 @@ const Login = () => {
 							/>
 						</div>
 						<div className="flex flex-col gap-1 relative">
-							<label className={`${
-								passError
-									? "text-red-500 font-medium"
-									: "text-gray-500 font-medium"
-							}`}>
+							<label
+								className={`${
+									passError ? "text-red-500 font-medium" : "text-gray-500 font-medium"
+								}`}>
 								Password
 							</label>
 							<input
