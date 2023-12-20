35fefedfbc1aa3ecb1dc3129e48bafbda1ae25b1
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-20 21:02:14 +0800

    added large screeen responsiveness

--- src/components/StudentStats.jsx
+++ src/components/StudentStats.jsx
@@ -1,74 +1,70 @@
 import { useEffect, useState } from "react";
 
+const StudentStats = ({
+	collegeAttend,
+	programAttend,
+	selectedProgram,
+	allStudents,
+}) => {
+	const [attendData, setAttendData] = useState([]);
 
-const StudentStats = ({collegeAttend, programAttend, selectedProgram, allStudents}) => {
-    
-    const [attendData, setAttendData] = useState([]);
+	useEffect(() => {
+		if (allStudents) {
+			setAttendData(collegeAttend);
+		} else if (selectedProgram) {
+			setAttendData(programAttend);
+		} else {
+			setAttendData(collegeAttend);
+		}
+		// setAttendData(!selectedProgram ? collegeAttend : programAttend);
+	}, [collegeAttend, programAttend, selectedProgram, allStudents]);
 
-    useEffect(() => {
-        if (allStudents) {
-            setAttendData(collegeAttend);
-        } else if (selectedProgram) {
-            setAttendData(programAttend);
-        } else {
-            setAttendData(collegeAttend);
-        }
-        // setAttendData(!selectedProgram ? collegeAttend : programAttend);
-    }, [collegeAttend, programAttend, selectedProgram, allStudents]);
+	const formatHours = (totalHours) => {
+		const hours = Math.floor(totalHours);
+		const minutes = Math.round((totalHours - hours) * 60);
+		return `${hours}:${String(minutes).padStart(2, "0")}`;
+	};
 
-    const formatHours = (totalHours) => {
-        const hours = Math.floor(totalHours);
-        const minutes = Math.round((totalHours - hours) * 60);
-        return `${hours}:${String(minutes).padStart(2, '0')}`;
-    };
-
-    return (
-        <tbody> 
-            {attendData?.map((data, key) => (
-                <tr key={key} className="bg-white border-b hover:bg-gray-200 text-xs    ">
-                    <td scope="row" className="px-6 py-4 font-medium  whitespace-nowrap">
-                        {data?.student_id}
-                    </td>
-                    <td className="px-6 py-4">
-                        {data?.student_first_name}
-                    </td>
-                    <td className="px-6 py-4">
-                        {data?.student_last_name}
-                    </td>
-                    <td className="px-6 py-4">
-                        {data?.program_name}
-                    </td>
-                    <td className="px-6 py-4">
-                        {data?.year_level_code}
-                    </td>
-                    <td className="px-6 py-4">
-                        {new Date(data?.time_in).toLocaleDateString("en-US", {
-                            hour: "numeric",
-                            minute: "2-digit",
-                            month: "long",
-                            day: "2-digit",
-                            year: "numeric",
-                        })}
-                    </td>
-                    <td className="px-6 py-4">
-                        {data?.time_out && new Date(data?.time_out).toLocaleDateString("en-US", {
-                            hour: "numeric",
-                            minute: "2-digit",
-                            month: "long",
-                            day: "2-digit",
-                            year: "numeric",
-                        })}
-                    </td>
-                    <td className="px-6 py-4">
-                        {formatHours(data?.total_hours)} hrs
-                    </td>
-                    <td className="px-6 py-4">
-                        {data?.admin_first_name} {data?.admin_last_name}
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-    );
+	return (
+		<tbody>
+			{attendData?.map((data, key) => (
+				<tr key={key} className="bg-white border-b hover:bg-gray-200 text-xs    ">
+					<td scope="row" className="px-6 py-4 font-medium  whitespace-nowrap">
+						{data?.student_id}
+					</td>
+					<td className="px-6 py-4">{data?.student_first_name}</td>
+					<td className="px-6 py-4">{data?.student_last_name}</td>
+					<td className="px-6 py-4">{data?.program_name}</td>
+					<td className="px-6 py-4">{data?.year_level_code}</td>
+					<td className="px-6 py-4">
+						{new Date(data?.time_in).toLocaleString("en-US", {
+							hour: "numeric",
+							minute: "2-digit",
+							month: "long",
+							day: "2-digit",
+							year: "numeric",
+							timeZone: "Asia/Manila", // Set the time zone to Philippine time
+						})}
+					</td>
+					<td className="px-6 py-4">
+						{data?.time_out &&
+							new Date(data?.time_out).toLocaleString("en-US", {
+								hour: "numeric",
+								minute: "2-digit",
+								month: "long",
+								day: "2-digit",
+								year: "numeric",
+								timeZone: "Asia/Manila", // Set the time zone to Philippine time
+							})}
+					</td>
+					<td className="px-6 py-4">{formatHours(data?.total_hours)} hrs</td>
+					<td className="px-6 py-4">
+						{data?.admin_first_name} {data?.admin_last_name}
+					</td>
+				</tr>
+			))}
+		</tbody>
+	);
 };
 
 export default StudentStats;
--- src/pages/interface/Dashboard.jsx
+++ src/pages/interface/Dashboard.jsx
@@ -95,7 +95,7 @@ const Dashboard = () => {
 	};
 
 	return (
-		<div className="flex flex-col mt-16 py-6 px-16">
+		<div className="flex flex-col mt-16 py-6 px-16 lg:max-w-7xl mx-auto">
 			<Toaster position="top-center" closeButton richColors />
 			<div className="fixed top-0 left-0 w-full bg-white shadow-md z-40 px-16 pt-6">
 				<Topbar />
@@ -144,7 +144,7 @@ const Dashboard = () => {
 				{/* <StudentStats programAttend={programAttend} /> */}
 			</div>
 			<hr />
-			<Statistics programInfo={programInfo}/>
+			<Statistics programInfo={programInfo} />
 			<div>
 				<StudentLists
 					programAttend={programAttend}
